e22150f1f8be2a803ec63cc1cc9ee2060b0ba31f
Ruslan Sovinskyy <ruslan.sovinskyy@gmail.com>
2024-03-30 12:11:29 +0000

    Listens to CAN BUS but blocks the display till frame is received

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate alloc;
 
 use alloc::format;
 use core::{borrow::Borrow, mem::MaybeUninit};
+use embedded_can::nb::Can;
 use embedded_graphics::{pixelcolor::Rgb565, prelude::*};
 use esp_backtrace as _;
 use esp_hal::{
@@ -13,17 +14,20 @@ use esp_hal::{
     peripherals::Peripherals,
     prelude::*,
     spi::{self, master::Spi},
-    Delay,
+    timer::TimerGroup,
+    twai::{self, filter::SingleStandardFilter},
+    Delay, Rng,
 };
-use esp_hal::{timer::TimerGroup, Rng};
 use esp_println::println;
 use esp_wifi::{initialize, EspWifiInitFor};
 use gc9a01::{mode::BufferedGraphics, prelude::*, Gc9a01, SPIDisplayInterface};
+use nb::block;
 use u8g2_fonts::{
     fonts,
     types::{FontColor, HorizontalAlignment, VerticalPosition},
     FontRenderer,
 };
+
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 
@@ -111,6 +115,40 @@ fn main() -> ! {
     display.init(&mut delay).ok();
     let mut lambda: u32 = 850;
 
+    // Set the tx pin as open drain. Skip this if using transceivers.
+    let can_tx_pin = io.pins.gpio0.into_open_drain_output();
+    let can_rx_pin = io.pins.gpio2;
+
+    // The speed of the CAN bus.
+    const CAN_BAUDRATE: twai::BaudRate = twai::BaudRate::B1000K;
+
+    // Begin configuring the TWAI peripheral. The peripheral is in a reset like
+    // state that prevents transmission but allows configuration.
+    let mut can_config = twai::TwaiConfiguration::new(
+        peripherals.TWAI0,
+        can_tx_pin,
+        can_rx_pin,
+        &clocks,
+        CAN_BAUDRATE,
+    );
+
+    // Partially filter the incoming messages to reduce overhead of receiving
+    // undesired messages. Note that due to how the hardware filters messages,
+    // standard ids and extended ids may both match a filter. Frame ids should
+    // be explicitly checked in the application instead of fully relying on
+    // these partial acceptance filters to exactly match.
+    // A filter that matches StandardId::ZERO.
+
+    // TODO: Uncomment this once we know the frames we're looking for
+    // const FILTER: SingleStandardFilter =
+    //     SingleStandardFilter::new(b"00000000000", b"x", [b"xxxxxxxx", b"xxxxxxxx"]);
+    // can_config.set_filter(FILTER);
+
+    // Start the peripheral. This locks the configuration settings of the peripheral
+    // and puts it into operation mode, allowing packets to be sent and
+    // received.
+    let mut can = can_config.start();
+
     loop {
         println!("Loop...");
         if lambda >= 1200 {
@@ -122,6 +160,10 @@ fn main() -> ! {
         display.clear();
         draw(&mut display, lambda);
         display.flush().ok();
+
+        // I think the frame I will be looking is 0X470 or something like that
+        let frame = block!(can.receive()).unwrap();
+        println!("Received a frame: {frame:?}");
 
         delay.delay_ms(50u32);
     }
