6121699321a169c2d2735a8324de5eb36004866b
Ruslan <ruslan.sovinskyy@gmail.com>
2024-08-24 02:50:49 +0000

    Get it building static content with slint and custom font / background

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,10 @@ which idf.py >/dev/null || {
     source ~/export-esp.sh >/dev/null 2>&1
 }
 
+# Need to tell which font sizes to include
+# https://github.com/slint-ui/slint/issues/4956
+# export SLINT_FONT_SIZES="8,10,12,14,16,18,20,32,42,60,100";
+
 case "$1" in
 "" | "release")
     cargo build --release
